916fe5b578ddebf28ab11ce59d26ab707a6007d8
Wayne Hofstra <wayne.hofstra@hva.nl>
2020-09-20 13:32:31 +0200

    Begin Node Editor

---
+++ Assets/Scripts/Dialogue/DialogueEditor.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class DialogueEditor : EditorWindow
+{
+    private List<Node> nodes;
+
+    private GUIStyle nodeStyle;
+
+    [MenuItem("Window/Custom Dialogue")]
+    private static void OpenWindow()
+    {
+        DialogueEditor window = GetWindow<DialogueEditor>();
+        window.titleContent = new GUIContent("Custom Dialogue");
+    }
+
+    private void OnEnable()
+    {
+        nodeStyle = new GUIStyle();
+        nodeStyle.normal.background = EditorGUIUtility.Load("builtin skins/darkskin/images/node3.png") as Texture2D;
+        nodeStyle.border = new RectOffset(12, 12, 12, 12);
+    }
+
+    private void OnGUI()
+    {
+        DrawNodes();
+        ProcessEvents(Event.current);
+
+        if (GUI.changed) {
+            Repaint();
+        }
+    }
+
+    private void DrawNodes()
+    {
+        if (nodes != null) {
+            for (int i = 0; i < nodes.Count; i++) {
+                nodes[i].Draw();
+            }
+        }
+    }
+
+    private void ProcessEvents(Event anEvent)
+    {
+        switch (anEvent.type)
+        {
+            case EventType.MouseDown:
+                if (anEvent.button == 1) {
+                    ProcessContextMenu(anEvent.mousePosition);
+                }
+                break;
+        }
+    }
+
+    private void ProcessContextMenu(Vector2 mousePosition)
+    {
+        GenericMenu genericMenu = new GenericMenu();
+        genericMenu.AddItem(new GUIContent("Add Node"), false, () => OnClickAddNode(mousePosition));
+        genericMenu.ShowAsContext();
+    }
+
+    private void OnClickAddNode(Vector2 mousePosition)
+    {
+        if (nodes == null) {
+            nodes = new List<Node>();
+        }
+
+        nodes.Add(new Node(mousePosition, 200, 50, nodeStyle));
+    }
+}
