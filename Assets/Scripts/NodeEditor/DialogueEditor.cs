ddd761d2ede5beea630f3fe748fdb4c7622807c6
Wayne Hofstra <wayne.hofstra@hva.nl>
2020-09-22 09:03:41 +0200

    Select and Move Nodes

--- Assets/Scripts/NodeEditor/DialogueEditor.cs
+++ Assets/Scripts/NodeEditor/DialogueEditor.cs
@@ -12,8 +12,15 @@ public class DialogueEditor : EditorWindow
     private NodeConnectionPoint selectedOutPoint;
 
     private GUIStyle nodeStyle;
+    private GUIStyle selectedStyle;
     private GUIStyle inPointStyle;
     private GUIStyle outPointStyle;
+
+    private Vector2 dragZero = new Vector2(0, 0);
+    private Vector2 drag;
+    private Vector2 offset;
+
+    private bool nodeDragging;
 
     [MenuItem("Window/Custom Dialogue")]
     private static void OpenWindow()
@@ -25,18 +32,23 @@ public class DialogueEditor : EditorWindow
 
     private void OnEnable()
     {
-        //Generates a New Node
+        //Generates a New Style for Node
         nodeStyle = new GUIStyle();
         nodeStyle.normal.background = EditorGUIUtility.Load("builtin skins/darkskin/images/node3.png") as Texture2D;
         nodeStyle.border = new RectOffset(12, 12, 12, 12);
 
-        //Generate Input
+        //Generates a Skin for a Selected Node
+        selectedStyle = new GUIStyle();
+        selectedStyle.normal.background = EditorGUIUtility.Load("builtin skins/darkskin/images/node3 on.png") as Texture2D;
+        selectedStyle.border = new RectOffset(12, 12, 12, 12);
+
+        //Generate Input Display
         inPointStyle = new GUIStyle();
         inPointStyle.normal.background = EditorGUIUtility.Load("builtin skins/darkskin/images/btn left.png") as Texture2D;
         inPointStyle.active.background = EditorGUIUtility.Load("builtin skins/darkskin/images/btn left on.png") as Texture2D;
         inPointStyle.border = new RectOffset(4, 4, 12, 12);
 
-        //Generate Output
+        //Generate Output Display
         outPointStyle = new GUIStyle();
         outPointStyle.normal.background = EditorGUIUtility.Load("builtin skins/darkskin/images/btn right.png") as Texture2D;
         outPointStyle.active.background = EditorGUIUtility.Load("builtin skins/darkskin/images/btn right on.png") as Texture2D;
@@ -45,9 +57,14 @@ public class DialogueEditor : EditorWindow
 
     private void OnGUI()
     {
+        //Background (Grid)
+        DrawGrid(20, 0.2f, Color.gray);
+        DrawGrid(100, 0.4f, Color.gray);
+
         //Draw
         DrawNodes();
         DrawConnections();
+        //DrawConnectionLine(Event.current);
 
         //Check for Input
         ProcessNodeEvents(Event.current);
@@ -56,6 +73,33 @@ public class DialogueEditor : EditorWindow
         if (GUI.changed) {
             Repaint();
         }
+    }
+
+    private void DrawGrid(float space, float opacity, Color gridColor)
+    {
+        int width = Mathf.CeilToInt(position.width / space);
+        int height = Mathf.CeilToInt(position.height / space);
+
+        Handles.BeginGUI();
+        Handles.color = new Color(gridColor.r, gridColor.g, gridColor.b, opacity);
+
+        offset += drag * 0.5f;
+        Vector3 newOffset = new Vector3(offset.x % space, offset.y % space, 0);
+
+        //Draw Vertical Lines
+        for (int i = 0; i < width; i++) {
+            Handles.DrawLine(new Vector3(space * i, -space, 0) + newOffset,
+                             new Vector3(space * i, position.height, 0f) + newOffset);
+        }
+
+        //Draw Horizontal Lines
+        for (int j = 0; j < height; j++) {
+            Handles.DrawLine(new Vector3(-space, space * j, 0) + newOffset,
+                             new Vector3(position.width, space * j, 0f) + newOffset);
+        }
+
+        Handles.color = Color.white;
+        Handles.EndGUI();
     }
 
     private void DrawNodes()
@@ -101,6 +145,12 @@ public class DialogueEditor : EditorWindow
                     ProcessContextMenu(anEvent.mousePosition);
                 }
                 break;
+
+            case EventType.MouseDrag:
+                if (anEvent.button == 0) {
+                    OnDrag(anEvent.delta);
+                }
+                break;
         }
     }
 
@@ -114,7 +164,10 @@ public class DialogueEditor : EditorWindow
 
     private void AddNode(Vector2 mousePosition)
     {
-        nodes.Add(new Node(mousePosition, 200, 50, nodeStyle, inPointStyle, outPointStyle, OnClickInPoint, OnClickOutPoint));
+        Node aNode = new Node(mousePosition, 200, 50, nodeStyle, selectedStyle, inPointStyle, outPointStyle,
+                              OnClickInPoint, OnClickOutPoint, RemoveNode);
+        aNode.Dragging += SetNodeDragging;
+        nodes.Add(aNode);
     }
 
     private void OnClickInPoint(NodeConnectionPoint inPoint)
@@ -149,6 +202,26 @@ public class DialogueEditor : EditorWindow
         }
     }
 
+    private void OnDrag(Vector2 delta)
+    {
+        drag = delta;
+        if (nodes != null)
+        {
+            //Move Nodes
+            for (int i = 0; i < nodes.Count; i++) {
+                nodes[i].Drag(delta);
+            }
+        }
+        GUI.changed = true;
+        Debug.Log("Dragging Grid");
+    }
+
+    private void SetNodeDragging()
+    {
+        nodeDragging = true;
+        //Debug.Log("Dragging Node");
+    }
+
     private void RemoveConnection(NodeConnection connection)
     {
         connections.Remove(connection);
@@ -157,6 +230,35 @@ public class DialogueEditor : EditorWindow
     private void CreateConnection()
     {
         connections.Add(new NodeConnection(selectedInPoint, selectedOutPoint, RemoveConnection));
+    }
+
+    private void RemoveNode(Node node)
+    {
+        //Before the Node Will be Removed, the Connections Need to be Removed First
+        if (connections != null)
+        {
+            //Initialize List of Node Connections to Remove
+            List<NodeConnection> connectionsToRemove = new List<NodeConnection>();
+
+            //Put Node Connections in Said List
+            for (int i = 0; i < connections.Count; i++)
+            {
+                if (connections[i].InPoint == node.InPoint || connections[i].OutPoint == node.OutPoint) {
+                    connectionsToRemove.Add(connections[i]);
+                }
+            }
+
+            //Delete Those Node Connections
+            for (int i = 0; i < connectionsToRemove.Count; i++) {
+                connections.Remove(connectionsToRemove[i]);
+            }
+
+            //Clear List of Node Connections to Remove
+            connectionsToRemove = null;
+        }
+
+        //Remove Node
+        nodes.Remove(node);
     }
 
     private void ClearConnectionSelection()
--- Assets/Scripts/NodeEditor/Node.cs
+++ Assets/Scripts/NodeEditor/Node.cs
@@ -1,33 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 using System;
 
 public class Node
 {
+    public Action Dragging;
+    public Action<Node> RemoveNode;
+
     private NodeConnectionPoint inPoint;
     private NodeConnectionPoint outPoint;
     private Rect rect;
+
     private GUIStyle style;
+    private GUIStyle defaultStyle;
+    private GUIStyle selectedStyle;
 
     private string title;
     private bool isDragging;
+    private bool isSelected;
 
     public NodeConnectionPoint InPoint { get { return inPoint; } set { value = inPoint; } }
     public NodeConnectionPoint OutPoint { get { return outPoint; } set { value = outPoint; } }
     public Rect Rectangle { get { return rect; } set { value = rect; } }
     public GUIStyle Style { get { return style; } set { value = style; } }
+    public GUIStyle DefaultStyle { get { return defaultStyle; } set { value = defaultStyle; } }
+    public GUIStyle SelectedStyle { get { return selectedStyle; } set { value = selectedStyle; } }
     public string Title { get { return title; } set { value = title; } }
     public bool IsDragging { get { return isDragging; } set { value = isDragging; } }
+    public bool IsSelected { get { return isSelected; } set { value = isSelected; } }
 
-    public Node(Vector2 position, float width, float height, GUIStyle nodeStyle, GUIStyle inPointStyle,
-                GUIStyle outPointStyle, Action<NodeConnectionPoint> OnClickInPoint, Action<NodeConnectionPoint> OnClickOutPoint)
+    public Node(Vector2 position, float width, float height, GUIStyle nodeStyle, GUIStyle selectedStyle, GUIStyle inPointStyle,
+                GUIStyle outPointStyle, Action<NodeConnectionPoint> OnClickInPoint, Action<NodeConnectionPoint> OnClickOutPoint,
+                Action<Node> OnClickRemoveNode)
     {
         rect = new Rect(position.x, position.y, width, height);
         style = nodeStyle;
 
         inPoint = new NodeConnectionPoint(this, Constants.ConnectionPoint.Type.In, inPointStyle, OnClickInPoint);
         outPoint = new NodeConnectionPoint(this, Constants.ConnectionPoint.Type.Out, outPointStyle, OnClickOutPoint);
+
+        defaultStyle = nodeStyle;
+        this.selectedStyle = selectedStyle;
+        RemoveNode = OnClickRemoveNode;
     }
 
     public void Drag(Vector2 delta)
@@ -49,14 +65,25 @@ public class Node
             case EventType.MouseDown:
                 if (anEvent.button == 0)
                 {
-                    if (rect.Contains(anEvent.mousePosition)) {
+                    if (rect.Contains(anEvent.mousePosition))
+                    {
                         //Drag When Clicking
                         isDragging = true;
+                        Dragging();
+
                         GUI.changed = true;
+                        isSelected = true;
+                        style = selectedStyle;
                     }
                     else {
                         GUI.changed = true;
+                        isSelected = false;
+                        style = defaultStyle;
                     }
+                }
+                else if (anEvent.button == 1 && isSelected && rect.Contains(anEvent.mousePosition)) {
+                    ProcessContextMenu();
+                    anEvent.Use();
                 }
                 break;
 
@@ -76,5 +103,20 @@ public class Node
         }
 
         return false;
+    }
+
+    private void ProcessContextMenu()
+    {
+        //Show Menu
+        GenericMenu genericMenu = new GenericMenu();
+        genericMenu.AddItem(new GUIContent("Remove"), false, OnClickRemoveNode);
+        genericMenu.ShowAsContext();
+    }
+
+    private void OnClickRemoveNode()
+    {
+        if (RemoveNode != null) {
+            RemoveNode(this);
+        }
     }
 }
